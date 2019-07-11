d65e107ae81368e02d649cbe5069297ce8989d1a
emanuelguerke <emanuelguerke@users.noreply.github.com>
2019-07-11 03:47:28 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ console.log("bot start");
 let Twit = require("twit");
 var config = require("./config")
 var T = new Twit(config);
+
+// twit something
+T.post('statuses/update', { status: 'Learning how twitter api works' },
+ function(err, data, response) {
+    console.log(data)
+  })
+//get data
+T.get('search/tweets', { q: 'banana since:2011-07-11', count: 100 }, 
+    function(err, data, response) {
+    console.log(data);
+  })
+
