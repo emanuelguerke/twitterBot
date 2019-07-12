8c625e0e087ff6193b37c85bd022d247cb430cc9
emanuelguerke <emanuelguerke@users.noreply.github.com>
2019-07-12 01:00:53 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,12 +3,38 @@ console.log("bot start");
 let Twit = require("twit");
 var config = require("./config")
 var T = new Twit(config);
+//user stream
+var stream = T.stream('user');
+//someone follows me
+stream.on('follow', followed);
+//event message
+function followed(eMsg) {
+  var name = eMsg.source.name;
+  var screenName = eMsg.source.screen_name;
+  twitIt(`@ ${screenName} Thanks for follow me`);
+}
 
-// twit something
-T.post('statuses/update', { status: 'Learning how twitter api works' },
- function(err, data, response) {
-    console.log(data)
-  })
+//twitIt();
+//setInterval(twitIt, 10000*60);
+
+// tweet something
+function twitIt(text) {
+  let randomNumber = Math.floor(Math.random()*200);
+
+  let tweet = {
+    status: text
+  }
+
+  T.post('statuses/update', tweet,
+    function (err, data, response) {
+      if(err){
+        console.log(err);
+      }else{
+        console.log(data)
+      }
+    })
+}
+
 //get data
 T.get('search/tweets', { q: 'banana since:2011-07-11', count: 100 }, 
     function(err, data, response) {
